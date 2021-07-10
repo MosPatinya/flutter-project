500686e768d2678e2031bb314f9fbaeff65484a2
patinya007 <85181997+patinya007@users.noreply.github.com>
2021-07-10 16:15:52 +0700

    123

--- lib/pages/Account.dart
+++ lib/pages/Account.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+
 class Account extends StatefulWidget {
   Account({Key key}) : super(key: key);
 
@@ -11,24 +15,56 @@ class _AccountState extends State<Account> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white30,
-       body: Center(
-         child: Column(
-           mainAxisAlignment: MainAxisAlignment.start,
-           children:<Widget> [
-             SizedBox(
-               height: 80,
-             ),
-             CircleAvatar(
-               backgroundImage: AssetImage('assets/images/acc.png'),
-               backgroundColor: Colors.blue,
-               radius : 50.0
-             ),
-             Padding(
-               padding: EdgeInsets.symmetric(vertical:15,horizontal: 15),
-               ),
-           ],
-         ),
-           ),
-         );
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 70,
+            ),
+            Profile(),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                color: Colors.grey[200],
+                onPressed: () {},
+                child: Row(
+                  children: [Container()],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Profile extends StatelessWidget {
+  const Profile({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 115,
+      width: 115,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage('assets/images/acc.png'),
+            backgroundColor: Colors.blue,
+            radius: 50.0,
+          ),
+        ],
+      ),
+    );
   }
 }
