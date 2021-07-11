1421c3cb6adbde6161c8dddf8709af0bac3abf9e
patinya007 <85181997+patinya007@users.noreply.github.com>
2021-07-11 19:01:16 +0700

    new

--- lib/pages/Account.dart
+++ lib/pages/Account.dart
@@ -1,7 +1,5 @@
-import 'dart:html';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/accountbutton.dart/login.dart';
 
 class Account extends StatefulWidget {
   Account({Key key}) : super(key: key);
@@ -26,18 +24,101 @@ class _AccountState extends State<Account> {
             SizedBox(
               height: 20,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: FlatButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)),
-                color: Colors.grey[200],
-                onPressed: () {},
-                child: Row(
-                  children: [Container()],
-                ),
-              ),
-            )
+            ProfileMenu(),
+            ProfileMenu1(),
+            ProfileMenu2(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileMenu extends StatelessWidget {
+  const ProfileMenu({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: FlatButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        color: Colors.grey[200],
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Login()),
+          );
+        },
+        child: Row(
+          children: [
+            Icon(Icons.login),
+            SizedBox(width: 20),
+            Expanded(
+              child:
+                  Text("Login", style: Theme.of(context).textTheme.bodyText1),
+            ),
+            Icon(Icons.arrow_forward_ios)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileMenu1 extends StatelessWidget {
+  const ProfileMenu1({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: FlatButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        color: Colors.grey[200],
+        onPressed: () {},
+        child: Row(
+          children: [
+            Icon(Icons.cancel),
+            SizedBox(width: 20),
+            Expanded(
+              child: Text("ล้างข้อมูล",
+                  style: Theme.of(context).textTheme.bodyText1),
+            ),
+            Icon(Icons.arrow_forward_ios)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileMenu2 extends StatelessWidget {
+  const ProfileMenu2({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: FlatButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        color: Colors.grey[200],
+        onPressed: () {},
+        child: Row(
+          children: [
+            Icon(Icons.local_grocery_store),
+            SizedBox(width: 20),
+            Expanded(
+              child: Text("ผู้ประกอบการ",
+                  style: Theme.of(context).textTheme.bodyText1),
+            ),
+            Icon(Icons.arrow_forward_ios)
           ],
         ),
       ),
@@ -59,10 +140,13 @@ class Profile extends StatelessWidget {
         fit: StackFit.expand,
         children: [
           CircleAvatar(
-            backgroundImage: AssetImage('assets/images/acc.png'),
-            backgroundColor: Colors.blue,
-            radius: 50.0,
-          ),
+              radius: 10,
+              backgroundColor: Colors.black,
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/acc.png'),
+                backgroundColor: Colors.white70,
+                radius: 55.0,
+              )),
         ],
       ),
     );
